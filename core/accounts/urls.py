b6ce9decd1055f23b3d7b470a97c6b04c1c2c0a5
Tony Lee <leegreen123@naver.com>
2023-06-10 18:44:40 +0900

    partial update profile data

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 
-app_name = "accounts"
-
 router = SimpleRouter(trailing_slash=False)
-router.register('accounts', views.AccountViewSet, basename='accounts')
+router.register(r'accounts', views.AccountViewSet, basename='accounts')
 
 urlpatterns = router.urls
---
+++ core/general/authentications.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAccountOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user or request.user.is_superuser
+
