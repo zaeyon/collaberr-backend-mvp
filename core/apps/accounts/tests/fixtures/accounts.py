86b9e32fc740dca773737dba22e95fc0170a8dbd
Tony Lee <leegreen123@naver.com>
2023-06-18 18:06:52 +0900

    pytest not done, added prod yml

--- core/apps/accounts/tests/fixtures/accounts.py
+++ core/apps/accounts/tests/fixtures/accounts.py
@@ -1,14 +1,14 @@
 import pytest
 from model_bakery import baker
-from rest_framework.test import APIClient
 
-@pytest.fixture
-def account(db):
-    account = baker.make('Account', username='testuser', password='test', email='testuser@test.com', id='87b81f2974b4a444')
-    print(account)
-    return account
-@pytest.fixture
-def api_client():
-    client = APIClient()
-    return client
 
+# @pytest.fixture
+# @pytest.mark.django_db
+# def account(db):
+#     return baker.make(
+#             'apps.accounts.Account',
+#             username='testuserbaker',
+#             password='test',
+#             email='baker@test.com',
+#             id='87b81f2974b4a444'
+#             )
--- core/apps/accounts/tests/test_rest_api.py
+++ core/apps/accounts/tests/test_rest_api.py
@@ -1,41 +1,63 @@
 import pytest
 from rest_framework.test import APIClient
-from model_bakery import baker
-from django.contrib.auth import get_user
+from ..models import Accounts
 
-@pytest.fixture
-def api_client():
-    client = APIClient()
-    return client
 
 @pytest.mark.django_db
-class TestAccountAPI:
-
-    def test_login(self, client, account):
-        response = client.post('/login/', {'email': "testuser@test.com", 'password': 'test'})
-        assert response.status_code == 200
-
-    def test_edit_profile(self, api_client, account):
-        api_client.post('/login/', {'email': "testuser@test.com", 'password': 'test'})
-
-        logged_in_user = get_user(api_client)
-        logged_in_user_id = logged_in_user.id
-        print(logged_in_user_id)
-        response = api_client.patch(f'/api/accounts/{logged_in_user_id}/', {'username': 'new_username'})
-        assert response.status_code == 200
-        assert response.data['username'] == 'new_username'
-
-    # def test_create_campaign(self, client, account):
-    #     client.force_authenticate(user=account)
-    #     response = client.post('/api/campaigns/', {'title': 'Test Campaign', 'description': 'Test Description'})
-    #     assert response.status_code == 201
-    #     assert response.data['title'] == 'Test Campaign'
-    #     assert response.data['description'] == 'Test Description'
-
-    # def test_edit_campaign(self, client, account):
-    #     client.force_authenticate(user=account)
-    #     response = client.patch(f'/api/campaigns/{account.id}/', {'title': 'Updated Campaign'})
-    #     assert response.status_code == 200
-    #     assert response.data['title'] == 'Updated Campaign'
+def test_client(api_client):
+    response = api_client.get('/api/login/')
+    assert response.status_code == 200
 
 
+@pytest.mark.django_db
+def test_login(api_client):
+    # Prepare the request data (e.g., username and password)
+    account = Accounts.objects.create_user(
+            username='testuserbaker',
+            password='test',
+            email='baker@test.com',
+            )
+    data = {
+        'email': 'baker@test.com',
+        'password': 'test'
+    }
+
+    # Send the POST request to the login endpoint
+    response = api_client.post('/api/login/', data=data, format='json')
+
+    # Assert the response status code
+    assert response.status_code == 200
+
+    # Verify the response data
+    response_data = response.json()
+    assert 'access' in response_data
+    assert 'refresh' in response_data
+
+
+# def test_login(api_client, account):
+#     response = api_client.post(
+#             '/api/login/',
+#             {'email': 'testuserbaker@test.com', 'password': 'test'},
+#             format='json')
+
+#     assert response.status_code == 200
+
+
+# def test_edit_profile(api_client, account):
+#     api_client.post('/api/login/', {
+#         'email': 'testuser@test.com',
+#         'password': 'test'
+#         })
+
+#     logged_in_user = get_user(api_client)
+#     logged_in_user_id = logged_in_user.id
+#     print(logged_in_user_id)
+#     response = api_client.patch(f'/api/accounts/{logged_in_user_id}/',
+#                                 {'username': 'new_username'})
+#     assert response.status_code == 200
+#     assert response.json() == {
+#         'username': 'new_username',
+#         'email': 'testuser@test.com',
+#         'first_name': '',
+#         'last_name': ''
+#         }
