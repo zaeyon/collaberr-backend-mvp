ac01ea55a35105bf18352d848c931bdce0a542e8
Tony Lee <leegreen123@naver.com>
2023-06-29 23:15:16 +0900

    store refresh in cookie

--- core/api/authentications/views.py
+++ core/api/authentications/views.py
@@ -6,6 +6,7 @@ from core.general.authentication import CustomJWTAuthentication
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework import status
 # drf_simplejwt imports
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -26,6 +27,7 @@ REDIRECT_URI = "http://localhost:8000/api/youtube/oauth2callback/"
 
 class CustomTokenRefreshView(TokenRefreshView):
     permission_classes = [IsAuthenticated]
+    authentication_classes = [CustomJWTAuthentication]
 
     def post(self, request, *args, **kwargs):
         refresh_token = request.COOKIES.get('refresh_token')
@@ -43,9 +45,9 @@ class CustomTokenRefreshView(TokenRefreshView):
         token_obj.access_expires_at = timezone.now() + settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME']
         token_obj.save()
 
-        response = Response({'access_token': access_token})
+        response = HttpResponse(status=status.HTTP_200_OK)
         # SECURITY WARNING: Don't store access_token in cookie in production
-        response.set_cookie('access_token', access_token, httponly=True)
+        response.set_cookie('access_token', access_token, httponly=True, secure=True, samesite='None')
         return response
 
 
